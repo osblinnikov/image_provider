69003c7f4347a17a8e9780c46dbbf0e55ff56d5b
Oleg Blinnikov <osblinnikov@gmail.com>
2021-06-21 22:53:22 +0200

    drop dummy component, as not necessary

--- hlscomponents/dummy.h
+++
@@ -1,8 +0,0 @@
-#ifndef IMAGE_PROVIDER_DUMMY_H_
-#define IMAGE_PROVIDER_DUMMY_H_
-
-#include "../image_provider/image_provider.h"
-
-component int image_provider_dummy_component() { return 0; }
-
-#endif // IMAGE_PROVIDER_DUMMY_H_
--- tests/tests.c
+++ tests/tests.c
@@ -1,4 +1,3 @@
-#include "../hlscomponents/dummy.h"
 #include "../image_provider/image_provider.h"
 
 int main() {
