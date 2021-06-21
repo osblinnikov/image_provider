c37a5d4e3ecdedd05d05e3245001c588a80d36b5
Oleg Blinnikov <osblinnikov@gmail.com>
2021-06-21 10:48:34 +0200

    use override config to pass compiler and linker arguments

--- SNocscript.py
+++ SNocscript.py
@@ -7,39 +7,29 @@ Import( 'env' )
 
 def with_gpp(env):
     # Replace current compiler with g++
-    args = copy.deepcopy(env['scons']['ARGUMENTS'])
-    arg_list = copy.deepcopy(env['scons']['ARGLIST'])
+    args = copy.deepcopy(env['prj_env']['ARGUMENTS'])
+    arg_list = copy.deepcopy(env['prj_env']['ARGLIST'])
 
     args['compiler'] = 'gpp'
     args['platform'] = 'x64'
     arg_list.append(('cppflag', '-std=c++11'))
 
-    nenv = prepare_env(args, arg_list)
-    
-    env['scons']['TOOLS']=nenv['TOOLS']
-    env['scons']['CC']=nenv['CC']
-    env['scons']['CPPFLAGS'].clear()
-    env['scons']['CPPFLAGS'].extend(nenv['CPPFLAGS'])
-    env['scons']['CCFLAGS'].clear()
-    env['scons']['CCFLAGS'].extend(nenv['CCFLAGS'])
-    env['scons']['CPPDEFINES'].clear()
-    env['scons']['CPPDEFINES'].extend(nenv['CPPDEFINES'])
-    
+    return prepare_env(args, arg_list)
 
-def with_opencv(env):
-    conf = Configure(env['scons'])
-    env['scons'].ParseConfig('pkg-config --cflags --libs opencv4')
-    env['scons'] = conf.Finish()
+def AddOpenCV(env):
+    conf = Configure(env['prj_env'])
+    env['prj_env'].ParseConfig('pkg-config --cflags --libs opencv4')
+    env['prj_env'] = conf.Finish()
 
 def add_dependencies(env):
+    AddOpenCV(env)
     # AddDependency(env,'libcaf_core','github.com/actor-framework/libcaf_core')
     # AddPthreads(env)
     # AddNetwork(env)
     # AddOpenGL(env)
     return
 
-with_gpp(env)
-with_opencv(env)
+nenv = with_gpp(env)
 
 c = {}
 c['PROG_NAME'] = 'image_provider'
@@ -50,4 +40,13 @@ c['defines'] = []
 c['depsDynamic'] = add_dependencies
 c['depsStatic'] = add_dependencies
 c['runnableOnly'] = False
+
+c['TOOLS'] = nenv['TOOLS']
+c['CC'] = nenv['CC']
+c['LINKFLAGS'] = nenv['LINKFLAGS']
+c['CCFLAGS'] = nenv['CPPFLAGS']
+c['CPPDEFINES'] = nenv['CPPDEFINES']
+c['LINKFLAGS'] = nenv['LINKFLAGS']
+c['LIBPATH'] = nenv['LIBPATH']
+
 DefaultLibraryConfig(env,c)
